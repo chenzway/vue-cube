f71267efff840d81e79b56f8a0b9bd223962b719
chenzway <599031437@qq.com>
2019-08-19 22:32:11 +0800

    modify home.vue

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,46 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link>| <router-link to="/about">About</router-link>|
-      <router-link to="/login" v-if="!isLogin">Login</router-link>
-      <a @click="logout" v-if="isLogin">logout</a>
-    </div>
-    <router-view />
+    <transition name="route-move">
+      <router-view class="child-view" />
+    </transition>
+    <cube-toolbar :actions="actions" @click="clickHandler"></cube-toolbar>
   </div>
 </template>
 <script>
 import { mapGetters } from 'vuex';
 
 export default {
+  data() {
+    return {
+      actions: [
+        {
+          text: '  Home',
+          icon: 'cubeic-home',
+          action: '/'
+        },
+        {
+          text: '  Cart',
+          icon: 'cubeic-mall',
+          action: '/about'
+        },
+        {
+          text: '  Me',
+          icon: 'cubeic-person ',
+          action: '/login'
+        }
+      ]
+    };
+  },
   methods: {
     logout() {
       this.$http.get('/api/logout').then(res => {
         const { code } = res.data;
         console.log(code);
       });
+    },
+    clickHandler(item) {
+      this.$router.push({ path: `${item.action}` });
+      console.log(item.action);
     }
   },
   computed: {
@@ -26,4 +49,28 @@ export default {
 };
 </script>
 
-<style lang="stylus"></style>
+<style lang="stylus">
+#app
+  /* 页面滑动动画 */
+  /* 入场前 */
+  .route-move-enter
+    transform translate3d(-100%, 0, 0)
+  /* 出场后 */
+  .route-move-leave-to
+    transform translate3d(100%, 0, 0)
+  .route-move-enter-active, .route-move-leave-active
+    transition transform 0.3s
+  .child-view
+    position absolute
+    left 0
+    top 0
+    width 100%
+    padding-bottom 36px
+  .cube-toolbar
+    bottom 0
+    width 100%
+    left 0
+    right 0
+    .cube-toolbar-item .cube-btn
+      font-size 0.42rem
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,41 @@
 <template>
-  <div class="home">Home</div>
+  <div class="home">
+    Home
+    <cube-button @click="showDrawer">Show Drawer</cube-button>
+    <cube-drawer
+      ref="drawer"
+      title="请选择"
+      :data="goods"
+      :selected-index="selectedIndex"
+      @change="changeHandler"
+      @select="selectHandler"
+      @cancel="cancelHandler"
+    ></cube-drawer>
+  </div>
 </template>
 
 <script>
 export default {
   name: 'home',
+  data() {
+    return {
+      selectedIndex: [],
+      goods: [], // 所有商品列表
+      drawerList: [] // 分类列表
+    };
+  },
   async created() {
     const res = await this.$http.get('/api/goods');
-    console.log(res.data);
+    this.goods = res.data.list;
+    console.log(this.goods);
+  },
+  methods: {
+    showDrawer() {
+      this.$refs.drawer.show();
+    },
+    changeHandler() {},
+    selectHandler() {},
+    cancelHandler() {}
   }
 };
 </script>
